48d245552561735a74f436d90bc435589bbe468b
jobiwankanobi <gymper@gmail.com>
2012-12-13 20:04:33 -0800

    Fix so that you can query 'roles'

--- parseQuery.php
+++ parseQuery.php
@@ -9,7 +9,7 @@ class parseQuery extends parseRestClient{
 	private $_include = array();
 
 	public function __construct($class=''){
-		if($class == 'users'){
+		if($class == 'users' || $class == 'roles'){
 			$this->_requestUrl = $class;
 		}
 		elseif($class != ''){
