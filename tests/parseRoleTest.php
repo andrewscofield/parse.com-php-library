bf852415e040885493ff32f84661b7f6f0b0a08a
jobiwankanobi <gymper@gmail.com>
2012-12-13 19:58:13 -0800

    added query to see if user is of a certain role.

--- tests/parseRoleTest.php
+++ tests/parseRoleTest.php
@@ -14,6 +14,10 @@ class parseRoleTest extends \Enhance\TestFixture {
     public $testUser;
     
     public function setUp(){
+        parseRestClient::$APPID = '22cASrmG1cWQ8LuV1kNRWjQjjpjf2rA9neNZMlLj';  // test app
+        parseRestClient::$MASTERKEY = 'WDXgAfDXybXJpswDacf5M4QzKT22Uxov85D12A8M';
+        parseRestClient::$RESTKEY = '6tKoG4XataK6FUKJEOdxjy4xAo9J4taNit0BnMfH';
+        parseRestClient::$PARSEURL = 'https://api.parse.com/1/';
         $this->parseRole = \Enhance\Core::getCodeCoverageWrapper('parseRole', array('TestRole'));
         $this->parseUser = new parseUser();
         $this->testUser = array(
@@ -74,11 +78,19 @@ class parseRoleTest extends \Enhance\TestFixture {
         \Enhance\Assert::isObject($return);
         \Enhance\Assert::isNotNull($return->updatedAt);
 
+        // Query parse role
+        $query = new parseQuery('users');
+        $query->whereRelatedTo('users', 'Role', $objectId);
+        $ret = $query->find();
+        \Enhance\Assert::isArray($ret);
+        \Enhance\Assert::isTrue(count($ret) == 1);
+
         $parseRole2->removeUsersFromRole(array($retUser->objectId));
         $return2 = $parseRole2->update($objectId);
         \Enhance\Assert::isObject($return2);
         \Enhance\Assert::isNotNull($return2->updatedAt);
-        
+
+        $parseRole2->delete($objectId);
     }
 }
 
